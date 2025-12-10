beda85712077b26919dea31e7f89dc5b5a264815
linyz0215 <1902829444@qq.com>
2025-12-10 15:39:36 +0800

    Initial commit of my Rust CLI tool

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Show CSVS, or Convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
+fn main() {
+    let opts = Opts::parse();
+    println!("{:#?}", opts);
+}
